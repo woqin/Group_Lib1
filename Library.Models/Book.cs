eaa82c1e81753b992b5ba58bd32230fcfe6b0c63
woqin <redmi1941@outlook.com>
2022-06-12 13:05:58 +0800

    搭建框架

---
+++ Library.Models/Book.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Models
+{
+    public class Book
+    {
+        public int BookId { get; set; }
+        public string BarCode { get; set; }
+        public string BookName { get; set; }
+        public string Author { get; set; }
+        public int PublisherId { get; set; }
+        public DateTime PublishDate { get; set; }
+        public int BookCategory { get; set; }
+        public double UnitPrice { get; set; }
+        public string BookImage { get; set; }
+        public int BookCount { get; set; }
+        public int Remainder { get; set; }
+        public string BookPosition { get; set; }
+        public DateTime RegTime { get; set; }
+
+        //扩展属性
+
+    }
+}
